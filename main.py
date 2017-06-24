1772146019ed9bf9b9b08798889f1f781e819032
elichad <eli.chadwick.256@gmail.com>
2017-06-24 13:24:28 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import video_streaming_with_flask_example/main
-
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  print("Hello world!")
+  return 'Hello, World!'
 
 if __name__ == '__main__':
   app.run()
