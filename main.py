c2c4fd2e9ad9b8ade62e4c8af57869759c0d5a41
elichad <eli.chadwick.256@gmail.com>
2017-06-24 13:20:21 +0100

    Maybe???

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import video_streaming_with_flask_example/main
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  python main.py
 
 if __name__ == '__main__':
   app.run()
