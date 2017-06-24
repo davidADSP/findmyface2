d520192457a4aa04b3d00a266a68f14c69b28df9
elichad <eli.chadwick.256@gmail.com>
2017-06-24 13:44:17 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -25,4 +25,4 @@ def video_feed():
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='http://webfront.azurewebsites.net/', debug=True)
